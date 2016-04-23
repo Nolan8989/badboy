1fccc1ae4f89bf919d76a0f2e3154b5bf5c55587
Nolan8989 <331639099@qq.com>
2016-04-23 19:19:39 +0800

    第一次提交

---
+++ my-web-server.js
@@ -0,0 +1,4 @@
+var http = requre('http');
+var serv = http.createServer(function(req,res){res.writeHead(200,{'Content-Type':'text/html'});
+res.end = ('<marquee>Smashing Node!</marquee');});
+serv.listen(3000);
